65b3869fbc4e6765a146687d4313a580198fd58f
hannachinassreddine <58274220+hannachinassreddine@users.noreply.github.com>
2020-09-28 22:32:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+/*alert("hello world!")*/
+function newDoc() {
+    window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    //window.location.assign("https://www.google.com")
+  }
+  if (confirm("tu veux aller ou site xxxvidsxxx")) newDoc()
+  
